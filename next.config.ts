ef980c254423807472fcd34e3955412edb0898fe
erik-cahya <erik.cahya841@gmail.com>
2025-02-03 17:54:24 +0800

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true, // Keep strict mode enabled
   experimental: {}, // Ensure there are no unnecessary keys
+  output: 'export',
 };
 
 export default nextConfig;
