90d1740120d1ff4b9110572a4191260b77c3f476
erik-cahya <erik.cahya841@gmail.com>
2025-02-03 18:31:03 +0800

    update: form contact updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,30 +8,43 @@ export default function Contact() {
       color: 'black'
     }
   }
-    return (
-      <div>
-        <h1>Contact Us</h1>
-        <form name="contact" method="POST" data-netlify="true">
-          <input type="hidden" name="form-name" value="contact" />
-          <div style={style.marginTop}>
-            <label>
-              Name: <input type="text" name="name" style={style.textBlack}/>
-            </label>
-          </div>
-          <div style={style.marginTop}>
-            <label>
-              Email: <input type="email" name="email" style={style.textBlack}/>
-            </label>
-          </div>
-          <div style={style.marginTop}>
-            <label>
-              Message: <textarea name="message" style={style.textBlack}></textarea>
-            </label>
-          </div>
-          <div style={style.marginTop}>
-            <button type="submit">Send</button>
-          </div>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <form 
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        {/* Hidden Field */}
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>
+            Don’t fill this out if you're human: 
+            <input name="bot-field" />
+          </label>
+        </p>
+
+        <div>
+          <label>
+            Name: <input type="text" name="name" required />
+          </label>
+        </div>
+        <div>
+          <label>
+            Email: <input type="email" name="email" required />
+          </label>
+        </div>
+        <div>
+          <label>
+            Message: <textarea name="message" required></textarea>
+          </label>
+        </div>
+        <div>
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    </div>
+  );
+}
