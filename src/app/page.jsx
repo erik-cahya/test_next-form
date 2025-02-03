4f54332a2d9c641b4c4447d7b846d0e9a5150060
erik-cahya <erik.cahya841@gmail.com>
2025-02-03 18:35:52 +0800

    update: page jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to My Next.js App</h1>
+      <h1>Welcome to My Next.js App Erik Cahya</h1>
       <Contact />
     </div>
   );
